e92646c020300b5486334b0ef36e4780f33591d4
key <1612730560@qq.com>
2017-09-20 00:05:10 +0800

    代码优化

--- onepiece.py
+++ onepiece.py
@@ -1,11 +1,12 @@
-import click
 import os
 import re
 import base64
 import json
-import requests
-import datetime
+import time
 import socket
+from urllib import parse
+import click
+import requests
 from multiprocessing.dummy import Pool as ThreadPool
 try:
     from lxml import etree
@@ -13,48 +14,63 @@ except:
     pass
 
 
-TIMEOUT = 15
-socket.setdefaulttimeout(TIMEOUT)
+socket.setdefaulttimeout(15)
 
 ILLEGAL_STR = r'\/:*?"<>|'
 REPLACE_ILLEGAL_STR = str.maketrans(ILLEGAL_STR, ' '*len(ILLEGAL_STR))
+HOST = 'http://ac.qq.com'
 
 
-def get_list(id):
+def get_all_chapter_by_lxml(id):
+    """根据漫画id获取所有的章节列表: http://ac.qq.com/Comic/ComicInfo/id/505430
+    args:
+        id: 505430
+
+    return:
+        [(chater1_url, chater1_title), (chater2_url, chater2_title),]
     """
-    根据URL获取章节列表
-    url类似:http://ac.qq.com/Comic/ComicInfo/id/505430
-    返回所有的章节信息
+    comic_id = str(id)
+    url = 'http://ac.qq.com/Comic/ComicInfo/id/{}'.format(id)
+    html = requests.get(url).text
+    tree = etree.HTML(html)
+    a_tags = tree.xpath("//ol[@class='chapter-page-all works-chapter-list']/li/p/span/a")
+    all_chapter = []
+    for a in a_tags:
+        title = a.get('title')
+        url = parse.urljoin(HOST, a.get('href'))
+        if comic_id not in url:
+            continue
+        all_chapter.append((title, url))
+    return all_chapter
+
+
+def get_all_chapter(id):
+    """根据漫画id获取所有的章节列表: http://ac.qq.com/Comic/ComicInfo/id/505430
+    args:
+        id: 505430
+
+    return:
+        [(chater1_url, chater1_title), (chater2_url, chater2_title),]
     """
     url = 'http://ac.qq.com/Comic/ComicInfo/id/{}'.format(id)
     html = requests.get(url).text
-    try:
-        tree = etree.HTML(html)
-        a_tag = tree.xpath(
-            "//ol[@class='chapter-page-all works-chapter-list']/li/p/span/a")
-        all_chapter = [{'url': 'http://ac.qq.com' +
-                        a.get('href'), 'title': a.get('title')} for a in a_tag]
-        all_chapter = list(
-            filter(lambda s: re.search(str(id), s['url']), all_chapter))
-        return all_chapter
-    except Exception as e:
-        ol = re.search(r'(<ol class="chapter-page-all works-chapter-list".+?</ol>)', html, re.S).group()
-        all_atag = re.findall(r'<a.+?>.+?</a>', ol, re.S)
-        all_chapter = []
-        for atag in all_atag:
-            title_res = re.search(r'title="(.+?)"', atag)
-            url_res = re.search(r'href="(.+?)"', atag)
-            if title_res and url_res:
-                title = title_res.group(1)
-                url = 'http://ac.qq.com' + url_res.group(1)
-                all_chapter.append({'url': url, 'title': title})
-        return all_chapter
+    ol = re.search(r'''(<ol class="chapter-page-all works-chapter-list".+?</ol>)''', html, re.S).group()
+    all_atag = re.findall(r'''<a.*?title="(.*?)".*?href="(.*?)">(.*?)</a>''', ol, re.S)
+    all_chapter = []
+    for item in all_atag:
+        title, url, _title = item
+        url = parse.urljoin(HOST, url)
+        all_chapter.append((title, url))
+    return all_chapter
 
 
 def get_detail_list(url):
-    """
-    根据详细URL获取章节详细页面并解析
-    url类似:http://ac.qq.com/ComicView/index/id/505430/cid/884
+    """根据章节的URL获取该章节的图片列表
+    args:
+        url: 章节的URL如 http://ac.qq.com/ComicView/index/id/505430/cid/884
+
+    return:
+        [pic1_url, pic2_url,]
     """
     pic_list = []
     html = requests.get(url).text
@@ -66,48 +82,54 @@ def get_detail_list(url):
 
 
 def download_chapter(chapter):
-    print('正在抓取图片地址 {0}'.format(chapter['title']))
-    chapter['pic_list'] = get_detail_list(chapter['url'])
-    comic_name = chapter['title'].split('：')[0]
-    title = ''.join(chapter['title'].split('：')[1:])
-    pic_list = chapter['pic_list']
-    dir_path = os.path.join(filter_filename(
-        comic_name), filter_filename(title))
+    """下载整个章节的图片，按漫画名按章节保存在当前目录下
+    """
+    title, chapter_url = chapter
+    print('正在下载', title)
+    chapter_pic_list = get_detail_list(chapter_url)
+    comic_name, chapter_title = title.split('：', 1)
+    dir_path = os.path.join(filter_filename(comic_name), filter_filename(chapter_title))
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
-    print('正在下载 {0}'.format(title))
-    for i in range(len(pic_list)):
+
+    for idx, img_url in enumerate(chapter_pic_list):
         try:
-            url = pic_list[i]
-            file_path = os.path.join(dir_path, str(i+1) + '.jpg')
-            if is_downloaded(file_path):
+            img_path = os.path.join(dir_path, str(idx+1) + '.jpg')
+            if os.path.exists(img_path) and os.path.getsize(img_path) != 0:
+                print('图片已存在, pass', img_path)
                 continue
-            img = requests.get(url).content
-            with open(file_path, 'wb') as f:
-                f.write(img)
+            with open(img_path, 'wb') as f:
+                f.write(requests.get(img_url).content)
         except Exception as e:
-            print('这张图片下载出错', title, pic_list[i])
+            print('这张图片下载出错', title, img_url)
 
 
 def filter_filename(filename):
-    '''windows文件名过滤非法字符串'''
+    """windows文件名过滤非法字符串
+    """
     return filename.translate(REPLACE_ILLEGAL_STR)
 
 
-def is_downloaded(file_path):
-    if os.path.exists(file_path) and os.path.getsize(file_path) != 0:
-        print('{0} 文件已存在,pass'.format(file_path))
-        return True
-    else:
-        return False
-
-
 def get_task_chapter(id, chapter, interval, mode):
-    all_chapter = get_list(id)
-    if mode and mode[0] in ['a','all']:
+    """根据参数来确定下载哪些章节
+    args:
+        id: 漫画id
+        chapter:
+            下载的哪个章节,如下载最后一个章节 chapter = -1
+        interval:
+            下载哪部分章节,如 interval = 1-10,20-30,66
+        mode:
+            mode = a or mode = all,则下载该漫画的所有章节
+
+    return:
+        [(chapter_i_title, chapter_i_url), (chapter_j_title, chapter_j_url)]
+    """
+    all_chapter = get_all_chapter(id)
+    if mode and mode[0] in ['a', 'all']:
         return all_chapter
     if not interval:
         return [all_chapter[chapter]]
+
     l = []
     blocks = re.split(r',|，', interval)
     for block in blocks:
@@ -123,14 +145,10 @@ def get_task_chapter(id, chapter, interval, mode):
 
 
 MSG = {
-    'id': '请输入你想要下载的漫画的id,默认下载海贼王\n\
-           如海贼王http://ac.qq.com/Comic/ComicInfo/id/505430\n\
-           python3 onepiece.py -id 50430',
-    'chapter': '输入要下载的章节chapter，默认下载最新章节\n\
-          如倒数第二 -c -2\n\
-        ',
-    'interval': '下载多个章节,输入章节区间,如 -i 1-10,25,30-40',
-    'thread': '线程池数,默认开启8个线程池,下载多个章节时效果才明显',
+    'id': '漫画id，海贼王: 505430 (http://ac.qq.com/Comic/ComicInfo/id/505430)',
+    'chapter': '要下载的章节chapter，默认下载最新章节。如 -c 666',
+    'interval': '要下载的章节区间, 如 -i 1-5,7,9-10',
+    'thread': '线程池数,默认开启8个线程池,下载多个章节时效果才明显'
 }
 
 
@@ -142,21 +160,33 @@ MSG = {
 @click.argument('mode', nargs=-1)
 def main(id, interval, chapter, thread, mode):
     """
-    根据腾讯漫画id下载图片
+    根据腾讯漫画id下载图片,默认下载海贼王最新一集。
 
-    添加 all/a 关键字即可下载所有章节
+    下载海贼王最新一集:
+    python3 onepiece.py
 
-    例如 python3 onepiece.py -id 50430 all
+    下载漫画 id=505430 所有章节:
+    python3 onepiece.py -id 505430 all
+
+    下载漫画 id=505430 第800集:
+    python3 onepiece.py -id 505430 -c 800
+
+    下载漫画 id=505430 倒数第二集:
+    python3 onepiece.py -id 505430 -c -2
+
+    下载漫画 id=505430 1到5集,7集，9到10集:
+    python3 onepiece.py -id 505430 -i 1-5,7,9-10
     """
     pool = ThreadPool(thread)
     task_chapter = get_task_chapter(id, chapter, interval, mode)
-    begin_time = datetime.datetime.now()
-    print('开始下载咯\n现在时间是:'+str(begin_time))
+    ts = time.time()
+    begin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(ts))
+    print('开始下载咯\n现在时间是:', begin_time)
     pool = ThreadPool(8)
     pool.map(download_chapter, task_chapter)
     pool.close()
     pool.join()
-    cost = (datetime.datetime.now() - begin_time).seconds
+    cost = int(time.time() - ts)
     print('下载完成啦\n下载用了这么长时间:{0}秒'.format(cost))
 
 
