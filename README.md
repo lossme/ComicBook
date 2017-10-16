f093de0b92445c19854f84b2e4f4abc2f9dd5023
key <1612730560@qq.com>
2017-10-16 12:08:46 +0800

    remove click

--- README.md
+++ README.md
@@ -2,34 +2,40 @@
 ## 腾讯漫画批量抓取
 
 
-#### 安装依赖
+### 安装依赖
 
 `python3 -m pip install -r requirements.txt`
 
 
-#### 使用帮助
+### 常规使用
+- 下载海贼王最新一集: python3 onepiece.py
+- 下载漫画 id=505430 最新一集: python3 onepiece.py -id 505430
+- 下载漫画 id=505430 所有章节: python3 onepiece.py -id 505430 all
+- 下载漫画 id=505430 第800集: python3 onepiece.py -id 505430 -c 800
+- 下载漫画 id=505430 倒数第二集: python3 onepiece.py -id 505430 -c -2
+- 下载漫画 id=505430 1到5集,7集，9到10集: python3 onepiece.py -id 505430 -i 1-5,7,9-10
+
+
+### 使用帮助
+
+
 ```
-Usage: onepiece.py [OPTIONS] [MODE]...
+>>>python3 onepiece.py --help
 
-  根据腾讯漫画id下载图片,默认下载海贼王最新一集。
+usage: onepiece.py [-h] [-id COMIC_ID] [-i INTERVAL] [-c CHAPTER] [-t THREAD]
+                   [-m MODE]
 
-  下载海贼王最新一集: python3 onepiece.py
+optional arguments:
+  -h, --help            show this help message and exit
+  -id COMIC_ID, --comic_id COMIC_ID
+                        漫画id，海贼王: 505430
+                        (http://ac.qq.com/Comic/ComicInfo/id/505430)
+  -i INTERVAL, --interval INTERVAL
+                        要下载的章节区间, 如 -i 1-5,7,9-10
+  -c CHAPTER, --chapter CHAPTER
+                        要下载的章节chapter，默认下载最新章节。如 -c 666
+  -t THREAD, --thread THREAD
+                        线程池数,默认开启8个线程池,下载多个章节时效果才明显
+  -m MODE, --mode MODE  下载模式，若为 a/all 则下载该漫画的所有章节, 如 -m all
 
-  下载漫画 id=505430 最新一集: python3 onepiece.py -id 505430
-
-  下载漫画 id=505430 所有章节: python3 onepiece.py -id 505430 all
-
-  下载漫画 id=505430 第800集: python3 onepiece.py -id 505430 -c 800
-
-  下载漫画 id=505430 倒数第二集: python3 onepiece.py -id 505430 -c -2
-
-  下载漫画 id=505430 1到5集,7集，9到10集: python3 onepiece.py -id 505430 -i 1-5,7,9-10
-
-Options:
-  -id, --id INTEGER      漫画id，海贼王: 505430
-                         (http://ac.qq.com/Comic/ComicInfo/id/505430)
-  -i, --interval TEXT    要下载的章节区间, 如 -i 1-5,7,9-10
-  -c, --chapter INTEGER  要下载的章节chapter，默认下载最新章节。如 -c 666
-  -t, --thread INTEGER   线程池数,默认开启8个线程池,下载多个章节时效果才明显
-  --help                 Show this message and exit.
 ```
--- onepiece.py
+++ onepiece.py
@@ -5,7 +5,7 @@ import json
 import time
 import socket
 from urllib import parse
-import click
+import argparse
 import requests
 from multiprocessing.dummy import Pool as ThreadPool
 try:
@@ -17,20 +17,20 @@ except:
 socket.setdefaulttimeout(15)
 
 ILLEGAL_STR = r'\/:*?"<>|'
-REPLACE_ILLEGAL_STR = str.maketrans(ILLEGAL_STR, ' '*len(ILLEGAL_STR))
+REPLACE_ILLEGAL_STR = str.maketrans(ILLEGAL_STR, ' ' * len(ILLEGAL_STR))
 HOST = 'http://ac.qq.com'
 
 
-def get_all_chapter_by_lxml(id):
+def get_all_chapter_by_lxml(comic_id):
     """根据漫画id获取所有的章节列表: http://ac.qq.com/Comic/ComicInfo/id/505430
-    args:
-        id: 505430
+    Args:
+        comic_id: 505430
 
-    return:
+    Returns:
         [(chater1_url, chater1_title), (chater2_url, chater2_title),]
     """
-    comic_id = str(id)
-    url = 'http://ac.qq.com/Comic/ComicInfo/id/{}'.format(id)
+    comic_id = str(comic_id)
+    url = 'http://ac.qq.com/Comic/ComicInfo/id/{}'.format(comic_id)
     html = requests.get(url).text
     tree = etree.HTML(html)
     a_tags = tree.xpath("//ol[@class='chapter-page-all works-chapter-list']/li/p/span/a")
@@ -44,15 +44,15 @@ def get_all_chapter_by_lxml(id):
     return all_chapter
 
 
-def get_all_chapter(id):
+def get_all_chapter(comic_id):
     """根据漫画id获取所有的章节列表: http://ac.qq.com/Comic/ComicInfo/id/505430
-    args:
+    Args:
         id: 505430
 
-    return:
+    Returns:
         [(chater1_url, chater1_title), (chater2_url, chater2_title),]
     """
-    url = 'http://ac.qq.com/Comic/ComicInfo/id/{}'.format(id)
+    url = 'http://ac.qq.com/Comic/ComicInfo/id/{}'.format(comic_id)
     html = requests.get(url).text
     ol = re.search(r'''(<ol class="chapter-page-all works-chapter-list".+?</ol>)''', html, re.S).group()
     all_atag = re.findall(r'''<a.*?title="(.*?)".*?href="(.*?)">(.*?)</a>''', ol, re.S)
@@ -66,10 +66,10 @@ def get_all_chapter(id):
 
 def get_detail_list(url):
     """根据章节的URL获取该章节的图片列表
-    args:
+    Args:
         url: 章节的URL如 http://ac.qq.com/ComicView/index/id/505430/cid/884
 
-    return:
+    Returns:
         [pic1_url, pic2_url,]
     """
     pic_list = []
@@ -98,7 +98,7 @@ def download_chapter(chapter):
 
     for idx, img_url in enumerate(chapter_pic_list):
         try:
-            img_path = os.path.join(dir_path, str(idx+1) + '.jpg')
+            img_path = os.path.join(dir_path, str(idx + 1) + '.jpg')
             if os.path.exists(img_path) and os.path.getsize(img_path) != 0:
                 print('图片已存在, pass', img_path)
                 continue
@@ -114,10 +114,10 @@ def filter_filename(filename):
     return filename.translate(REPLACE_ILLEGAL_STR)
 
 
-def get_task_chapter(id, chapter, interval, mode):
+def get_task_chapter(comic_id, chapter, interval, mode):
     """根据参数来确定下载哪些章节
-    args:
-        id: 漫画id
+    Args:
+        comic_id: 漫画id
         chapter:
             下载的哪个章节,如下载最后一个章节 chapter = -1
         interval:
@@ -125,13 +125,14 @@ def get_task_chapter(id, chapter, interval, mode):
         mode:
             mode = a or mode = all,则下载该漫画的所有章节
 
-    return:
+    Returns:
         [(chapter_i_title, chapter_i_url), (chapter_j_title, chapter_j_url)]
     """
-    all_chapter = get_all_chapter(id)
+    all_chapter = get_all_chapter(comic_id)
     if mode and mode[0] in ['a', 'all']:
         return all_chapter
     if not interval:
+        chapter = chapter - 1 if chapter > 0 else chapter
         return [all_chapter[chapter]]
 
     task_chapter = []
@@ -145,12 +146,12 @@ def get_task_chapter(id, chapter, interval, mode):
                 print('参数写错了,查看帮助: python3 onepiece.py --help')
                 exit()
             if chapter_int <= all_chapter_len:
-                task_chapter.append(all_chapter[chapter_int-1])
+                task_chapter.append(all_chapter[chapter_int - 1])
         else:
             start, end = block.split('-', 1)
             try:
                 start, end = int(start), int(end)
-                for chapter in all_chapter[start-1:end]:
+                for chapter in all_chapter[start - 1:end]:
                     task_chapter.append(chapter)
             except ValueError:
                 print('参数写错了,查看帮助: python3 onepiece.py --help')
@@ -158,44 +159,9 @@ def get_task_chapter(id, chapter, interval, mode):
     return task_chapter
 
 
-MSG = {
-    'id': '漫画id，海贼王: 505430 (http://ac.qq.com/Comic/ComicInfo/id/505430)',
-    'chapter': '要下载的章节chapter，默认下载最新章节。如 -c 666',
-    'interval': '要下载的章节区间, 如 -i 1-5,7,9-10',
-    'thread': '线程池数,默认开启8个线程池,下载多个章节时效果才明显'
-}
-
-
-@click.command()
-@click.option('-id', '--id', default=505430, help=MSG['id'])
-@click.option('-i', '--interval', default='', help=MSG['interval'])
-@click.option('-c', '--chapter', default=-1, help=MSG['chapter'])
-@click.option('-t', '--thread', default=8, help=MSG['thread'])
-@click.argument('mode', nargs=-1)
-def main(id, interval, chapter, thread, mode):
-    """
-    根据腾讯漫画id下载图片,默认下载海贼王最新一集。
-
-    下载海贼王最新一集:
-    python3 onepiece.py
-
-    下载漫画 id=505430 最新一集:
-    python3 onepiece.py -id 505430
-
-    下载漫画 id=505430 所有章节:
-    python3 onepiece.py -id 505430 all
-
-    下载漫画 id=505430 第800集:
-    python3 onepiece.py -id 505430 -c 800
-
-    下载漫画 id=505430 倒数第二集:
-    python3 onepiece.py -id 505430 -c -2
-
-    下载漫画 id=505430 1到5集,7集，9到10集:
-    python3 onepiece.py -id 505430 -i 1-5,7,9-10
-    """
+def main(comic_id=505430, interval=None, chapter=-1, thread=8, mode=None):
     pool = ThreadPool(thread)
-    task_chapter = get_task_chapter(id, chapter, interval, mode)
+    task_chapter = get_task_chapter(comic_id, chapter, interval, mode)
     ts = time.time()
     begin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(ts))
     print('开始下载咯\n现在时间是:', begin_time)
@@ -207,5 +173,43 @@ def main(id, interval, chapter, thread, mode):
     print('下载完成啦\n下载用了这么长时间:{0}秒'.format(cost))
 
 
+def cli():
+    """
+    根据腾讯漫画id下载图片,默认下载海贼王最新一集。
+
+    下载海贼王最新一集:
+    python3 onepiece.py
+
+    下载漫画 id=505430 最新一集:
+    python3 onepiece.py -id 505430
+
+    下载漫画 id=505430 所有章节:
+    python3 onepiece.py -id 505430 -m all
+
+    下载漫画 id=505430 第800集:
+    python3 onepiece.py -id 505430 -c 800
+
+    下载漫画 id=505430 倒数第二集:
+    python3 onepiece.py -id 505430 -c -2
+
+    下载漫画 id=505430 1到5集,7集，9到10集:
+    python3 onepiece.py -id 505430 -i 1-5,7,9-10
+    """
+    MSG = {
+        'comic_id': '漫画id，海贼王: 505430 (http://ac.qq.com/Comic/ComicInfo/id/505430)',
+        'chapter': '要下载的章节chapter，默认下载最新章节。如 -c 666',
+        'interval': '要下载的章节区间, 如 -i 1-5,7,9-10',
+        'thread': '线程池数,默认开启8个线程池,下载多个章节时效果才明显',
+        'mode': '下载模式，若为 a/all 则下载该漫画的所有章节, 如 -m all'
+    }
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-id', '--comic_id', type=int, default=505430, help=MSG['comic_id'])
+    parser.add_argument('-i', '--interval', type=str, help=MSG['interval'])
+    parser.add_argument('-c', '--chapter', type=int, default=-1, help=MSG['chapter'])
+    parser.add_argument('-t', '--thread', type=int, default=8, help=MSG['thread'])
+    parser.add_argument('-m', '--mode', type=str, help=MSG['mode'])
+    args = parser.parse_args()
+    main(comic_id=args.comic_id, interval=args.interval, chapter=args.chapter, thread=args.thread, mode=args.mode)
+
 if __name__ == "__main__":
-    main()
+    cli()
