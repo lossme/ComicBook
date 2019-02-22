fa9c5cf77f9e7b7f99ae20497d7ba1b28b01bc47
lossme <1612730560@qq.com>
2019-02-22 11:29:21 +0800

    api index

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 def create_app():
@@ -8,4 +8,22 @@ def create_app():
     from .views import app as main_app
     app.register_blueprint(main_app)
 
+    app.add_url_rule("/", "index", index)
+
     return app
+
+
+def index():
+    return jsonify(
+        {
+            "api_status": "ok",
+            "example": [
+                "/api/ishuhui/1",
+                "/api/ishuhui/1/933",
+                "/api/qq/505430",
+                "/api/qq/505430/933",
+                "/api/wangyi/5015165829890111936",
+                "/api/wangyi/5015165829890111936/933",
+            ]
+        }
+    )
