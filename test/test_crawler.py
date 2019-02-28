6e70c368e5f2db99c7ae1ac36a370cb378f7c17a
lossme <1612730560@qq.com>
2019-02-28 09:14:22 +0800

    test search

--- test/test_crawler.py
+++ test/test_crawler.py
@@ -15,29 +15,46 @@ def test_qq():
     # 海贼王  URL: https://ac.qq.com/Comic/ComicInfo/id/505430
     site = "qq"
     comicid = "505430"
+    name = "海贼王"
     chapter_number = -1
     crawl_comicbook(site=site, comicid=comicid, chapter_number=chapter_number)
+
+    result = ComicBook.search(site=site, name=name)
+    assert len(result) > 0
 
 
 def test_ishuhui():
     # 海贼王 URL: https://www.ishuhui.com/comics/anime/1
     site = "ishuhui"
     comicid = "1"
+    name = "海贼王"
     chapter_number = -1
     crawl_comicbook(site=site, comicid=comicid, chapter_number=chapter_number)
+
+    result = ComicBook.search(site=site, name=name)
+    assert len(result) > 0
 
 
 def test_wangyi():
     # 海贼王 URL: https://manhua.163.com/source/5015165829890111936
     site = "wangyi"
     comicid = "5015165829890111936"
+    name = "海贼王"
+
     chapter_number = -1
     crawl_comicbook(site=site, comicid=comicid, chapter_number=chapter_number)
+
+    result = ComicBook.search(site=site, name=name)
+    assert len(result) > 0
 
 
 def test_u17():
     # 雏蜂 URL: http://www.u17.com/comic/195.html
     site = "u17"
     comicid = "195"
+    name = "雏蜂"
     chapter_number = -1
     crawl_comicbook(site=site, comicid=comicid, chapter_number=chapter_number)
+
+    result = ComicBook.search(site=site, name=name)
+    assert len(result) > 0
