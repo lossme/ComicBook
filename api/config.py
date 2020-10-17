1361ba32d3a924299df09343c4c3b6bea7bf5ad6
key <1612730560@qq.com>
2020-10-17 20:01:12 +0800

    update config

--- api/config.py
+++ api/config.py
@@ -3,6 +3,18 @@ class Config(object):
     JSON_AS_ASCII = False
     LOG_LEVEL = 'INFO'
 
+
+class ProductionConfig(Config):
+    DEBUG = False
+    LOG_LEVEL = 'DEBUG'
+    URL_PREFIX = "http://127.0.0.1:8000"
+    DEFAULT_PROXY = 'socks5://127.0.0.1:1080'
+    CRAWLER_PROXY = {}
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    LOG_LEVEL = 'DEBUG'
     URL_PREFIX = "http://127.0.0.1:5000"
     DEFAULT_PROXY = 'socks5://127.0.0.1:1082'
 
@@ -12,13 +24,3 @@ class Config(object):
         'nhentai': DEFAULT_PROXY,
         'wnacg': DEFAULT_PROXY,
     }
-
-
-class ProductionConfig(Config):
-    DEBUG = False
-    LOG_LEVEL = 'DEBUG'
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    LOG_LEVEL = 'DEBUG'
